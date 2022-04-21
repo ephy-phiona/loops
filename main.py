9b41e1b1027ad97626db51b9dfeedfc00b24289d
ephy-phiona <ephyphiona@gmail.com>
2022-04-21 20:33:30 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from calendar import THURSDAY
+from pytz import country_names
+
+
+countries= ["Kenya", "Uganda", "Tanzania", "Rwanda"]
+for place in countries:
+  print(place) 
+  cities_=["Nairobi", "Kampala", "Dodoma", "Kigali"]
+  print(cities_)
+   
+  
+for number in range(4):
+    print("Hello there, hope you had a fantastic day.", number)
+    
+    for fruits in range(1,10):
+        print(["Mangoes","Oranges"],["Apples","Pineapple"],["Ovacado","Peach"])
+        print("lemon","berries","guava","kiwi")
+
